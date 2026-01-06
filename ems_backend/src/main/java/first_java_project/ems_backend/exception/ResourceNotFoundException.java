c23ff29406b777fabc6f7bf2aa4ce6debba1a18a
Diliprajan-N <diliprajan220@gmail.com>
2026-01-06 17:49:18 +0530

    Delete ems_backend directory

--- ems_backend/src/main/java/first_java_project/ems_backend/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package first_java_project.ems_backend.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-
-
-        }
-    }
